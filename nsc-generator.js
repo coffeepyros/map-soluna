23fdf1af0c59851e3348abee2da56edec4b5b5c3
coffeepyros <coffeepyros@gmail.com>
2020-12-02 18:39:32 +0100

    nsc editor

--- nsc-generator.js
+++ nsc-generator.js
@@ -1,3 +1,80 @@
+let nscData = {};
+
+function render() {
+  document.querySelector("#statblock .name").innerText = nscData.name;
+  document.querySelector("#statblock .bloodline").innerText = nscData.bloodline;
+}
+
+function handleChange(e) {
+  nscData = { ...nscData, [e.target.id]: e.target.value };
+  render();
+}
+
+const inputArray = ["name", "bloodline"];
+
+inputArray.map((id) =>
+  document.getElementById(id).addEventListener("keyup", handleChange)
+);
+
+// CLASS CHANGE
+
+function handleClassChange(e) {
+  let nscClass,
+    nscLevel = parseInt(document.getElementById("level").value);
+  if (e.target.id === "class") {
+    nscClass = e.target.value;
+  } else if (e.target.id === "level") {
+    nscLevel = e.target.value;
+  }
+
+  if (nscClass != "NSC" && nscClass != "Monster") {
+    // CLASS RELATED SAVING THROWS
+    let url;
+    url = "https://www.dnd5eapi.co/api/classes/" + e.target.value.toLowerCase();
+    axios({
+      method: "GET",
+      url: url,
+    })
+      .then((apiRes) => {
+        //  console.log(apiRes.data);
+        let output = [];
+        apiRes.data.saving_throws.map((st) => output.push(st.name));
+        document.getElementById("saving_throws").innerHTML =
+          "<strong>Saving Throws:</strong> " + output.join(", ");
+      })
+      .catch((err) => console.error(err.message));
+
+    // CLASS RELATED FEATURES
+    url =
+      "https://www.dnd5eapi.co/api/classes/" +
+      e.target.value.toLowerCase() +
+      "/levels";
+    axios({
+      method: "GET",
+      url: url,
+    })
+      .then((apiRes) => {
+        // console.log(apiRes.data);
+        let output = "";
+        let features = apiRes.data.filter((feat) => feat.level <= nscLevel);
+        // for each level
+        features.map((featureArray) => {
+          output += `<p><strong>Level ${featureArray.level}:</strong> `;
+          let temp = [];
+          featureArray.features.map((featureByLevels) => {
+            temp.push(featureByLevels.name);
+          });
+          output += `${temp.join(", ")}</p>`;
+        });
+        console.log(features);
+        document.getElementById("class_levels").innerHTML = output;
+      })
+      .catch((err) => console.error(err.message));
+  }
+}
+
+document.getElementById("class").addEventListener("change", handleClassChange);
+
 const abilities = ["str", "dex", "con", "int", "wis", "cha"];
 for (let stat of abilities) {
   document
